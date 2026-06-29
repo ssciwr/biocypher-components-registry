01912e8717a7cc2cf79e09f243043143432649a2
jmsssc <sk353@uni-heidelberg.de>
2026-06-29 17:39:01 +0200

    Update ESLint ignore config

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'src/api/client']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
