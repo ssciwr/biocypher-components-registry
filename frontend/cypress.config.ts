422cbe64b7dc5e5b39856815356688a5e13e6209
jmsssc <sk353@uni-heidelberg.de>
2026-06-29 18:22:59 +0200

    fix cypress config

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -4,5 +4,14 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
     specPattern: 'cypress/e2e/**/*.cy.ts',
+    supportFile: false, // manually added as we don't need it for now with just mocked backend anyhow.
   },
 })
+
+// due to this error:
+/*
+Your project does not contain a default supportFile. We expect a file matching cypress/support/e2e.{js,jsx,ts,tsx} to exist.
+
+If a support file is not necessary for your project, set supportFile to false.
+
+ */
