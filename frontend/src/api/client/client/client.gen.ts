634cc97ca86e1c5c010cfd98472ebd612e05d5fb
jmsssc <sk353@uni-heidelberg.de>
2026-06-29 18:41:34 +0200

    Remove todo to keep SonarQube happy later

--- frontend/src/api/client/client/client.gen.ts
+++ frontend/src/api/client/client/client.gen.ts
@@ -211,7 +211,6 @@ export const createClient = (config: Config = {}): Client => {
         throw finalError;
       }
 
-      // TODO: we probably want to return error and improve types
       return responseStyle === 'data'
         ? undefined
         : {
