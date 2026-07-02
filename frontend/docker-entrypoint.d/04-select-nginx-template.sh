6889a659288163a345928cb8221692a48593ca0d
jmsssc <sk353@uni-heidelberg.de>
2026-07-02 14:10:19 +0200

    Fix SonarQube duplication false positive due to OpenAPI-ts

--- frontend/docker-entrypoint.d/04-select-nginx-template.sh
+++ frontend/docker-entrypoint.d/04-select-nginx-template.sh
@@ -3,7 +3,7 @@ set -eu
 
 mkdir -p /etc/nginx/templates
 
-if [ -f "${TLS_CERTIFICATE_PATH}" ] && [ -f "${TLS_CERTIFICATE_PRIV_PATH}" ]; then
+if [ -r "${TLS_CERTIFICATE_PATH}" ] && [ -r "${TLS_CERTIFICATE_PRIV_PATH}" ]; then
     cp /etc/nginx/template-options/default.conf.https.template /etc/nginx/templates/default.conf.template
 else
     cp /etc/nginx/template-options/default.conf.http.template /etc/nginx/templates/default.conf.template
